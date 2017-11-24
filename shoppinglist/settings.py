b1e3ab9a731281aecca2edec1eeadc2d050debb9
toshiki kamei <toshiki@toshikis-MBPr.local>
2017-11-24 14:34:22 +0900

    debug False

--- shoppinglist/settings.py
+++ shoppinglist/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '!ul74wup=^^ez(vvgz4peu00q!2$y!$up-@u1l$i0oz+k56l5q'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
-
+DEBUG404 = True
 ALLOWED_HOSTS = ['*']
 
 
