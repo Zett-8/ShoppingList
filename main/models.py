184b54a368452de9840fa57238296bea3e004a22
toshiki kamei <toshiki@toshikis-MBPr.local>
2017-11-28 20:48:18 +0900

    new

--- main/models.py
+++ main/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 
 class Stuff(models.Model):
     title = models.CharField(max_length=100)
-    user = models.ForeignKey(User)
+    user = models.CharField(default=User, max_length=100)
 
     def __str__(self):
         return self.user
